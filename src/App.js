01f13fcf84241bdd0eaea599d2299d9942e28a40
griffenb <griffen2020@gmail.com>
2024-05-05 14:23:49 -0700

    fixed formatting stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
 import "./App.css";
-import profilePic from "./assets/Profile Pic Group.svg"; // Corrected import path
+import profilePic from "./assets/Profile Pic Group.svg";
 import fallenStarHome from "./assets/Fallen Star Home Frame.svg";
-//import cloudGroup from "./components/Cloud Group.svg";
+import cloudGroup from "./assets/Cloud Group.svg";
 
 function App() {
   return (
-    <div className="App">
-      <div className="img-overlay-wrap">
+    <div className="text">
+      <div className="profile-img">
         <img src={profilePic} alt="Profile" className="profile-img" />
+      </div>
+
+      <div className="clouds">
+        <img src={cloudGroup} alt="Cloud Group" />
       </div>
       <div className="name">Griffen Bengard</div>
       <div className="sub">UX Design</div>
-      <div className="fallen">
-        <img src={fallenStarHome} alt="Fallen Star Home" className="fallen" />
-      </div>
+      <img src={fallenStarHome} alt="Fallen Star Home" className="fallen" />
     </div>
   );
 }
