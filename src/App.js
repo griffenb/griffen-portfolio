83857cbc75ec567c814a25d85c1d200f135b9616
griffenb <griffen2020@gmail.com>
2024-05-07 10:28:10 -0700

    added hover, tried to add clouds

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import profilePic from "./assets/Profile Pic Group.svg";
 import fallenStarHome from "./assets/Fallen Star Home Frame.svg";
+import fallenStarHover from "./assets/Fallen Star Hover Page.svg";
 import cloudGroup from "./assets/Cloud Group.svg";
+import cloudGroupVid from "./assets/Clouds Group Video.mp4";
+import CloudsGroupGif from "./assets/Clouds Group Gif.gif";
 
 function App() {
+  const [isHovered, setIsHovered] = useState(false);
+  const [cloudsHovered, setCloudsHovered] = useState(false);
+
   return (
     <div className="text">
       <div className="profile-img">
@@ -12,11 +18,26 @@ function App() {
       </div>
 
       <div className="clouds">
-        <img src={cloudGroup} alt="Cloud Group" />
+        <gif
+          autoPlay
+          loop
+          muted
+          className="clouds-image"
+          onMouseEnter={() => setCloudsHovered(true)}
+          onMouseLeave={() => setCloudsHovered(false)}
+        >
+          <source src={CloudsGroupGif} type="gif" />
+        </gif>
       </div>
       <div className="name">Griffen Bengard</div>
       <div className="sub">UX Design</div>
-      <img src={fallenStarHome} alt="Fallen Star Home" className="fallen" />
+      <img
+        src={isHovered ? fallenStarHover : fallenStarHome}
+        alt="Fallen Star Home"
+        className="fallen"
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      />
     </div>
   );
 }
